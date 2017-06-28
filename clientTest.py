4a238206612af71265e7418849f4754121c1a437
zhangshuaishuai139 <2468435837@qq.com>
2017-06-28 15:38:14 +0800

    modify clientTest.py port 9034

--- clientTest.py
+++ clientTest.py
@@ -13,7 +13,7 @@ socket 客户端测试程序
 if __name__ == "__main__":
 
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.connect(("localhost", 8888))
+    socket.connect(("localhost", 9034))
     socket.send("大熊猫喜欢吃什么东西？")
     data = str(socket.recv(1024))
     print data.decode("utf8")
